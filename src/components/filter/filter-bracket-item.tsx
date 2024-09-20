e4c1940e190944d884fc081bcc129e26f71daa72
Stéphane Bisinger <stephane@sbisinger.ch>
2024-09-20 23:52:20 +0200

    Highlight selected brackets when image is missing

--- src/components/filter/filter-bracket-item.tsx
+++ src/components/filter/filter-bracket-item.tsx
@@ -1,12 +1,16 @@
 import styles from './filter-bracket-item.module.css';
 
 export default function FilterBracketItem({ imageSrc, value, onChange, name }) {
+    let textClass = styles['item-text'];
+    if (!imageSrc && !!value) {
+        textClass = `${textClass} ${styles['item-text--selected']}`;
+    }
     return (
         <div className={styles['bracket-item']} onClick={() => onChange(!value)}>
             {imageSrc ?
                 <img className={value ? `${styles['bracket-img']} ${styles['bracket-img--selected']}` : styles['bracket-img']} src={imageSrc}></img> : undefined}
             <div>
-                <span className={styles['item-text']}>{name}</span>
+                <span className={textClass}>{name}</span>
             </div>
         </div>
     );
