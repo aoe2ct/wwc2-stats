6ce300712c1bbd8f832c170418eccff06c345cbb
Stéphane Bisinger <stephane@sbisinger.ch>
2024-11-30 20:03:40 +0100

    Distinguish player map picks from admin picks

--- src/components/charts/map-pick-chart.tsx
+++ src/components/charts/map-pick-chart.tsx
@@ -6,33 +6,59 @@ import { merge } from 'lodash-es';
 import { Filter } from "../filter/filter-dialog";
 import { FilterLegendConfig } from "@site/src/utils/civ-chart-config";
 
-export default function MapPickChart({draftsData, filter}: {draftsData: {mapDrafts: any[]}, filter: Filter}): JSX.Element {
+export default function MapPickChart({ draftsData, filter }: { draftsData: { mapDrafts: any[] }, filter: Filter }): JSX.Element {
     useDelayedColorMode();
-    const draftPickData: {[key: string]: number} = draftsData.mapDrafts.reduce(
+    const draftPickData: { [key: string]: { player: number, admin: number } } = draftsData.mapDrafts.reduce(
         (acc, draft) => {
-            const mapPicks = draft.draft.filter(v => v.action === 'pick').map(v => v.map);
+            const mapPicks = draft.draft.filter(v => v.action === 'pick');
             for (const pick of mapPicks) {
-                if (acc.hasOwnProperty(pick)) {
-                    acc[pick] = acc[pick] + 1;
-                } else {
-                    acc[pick] = 1;
+                const mapName = pick.map;
+                if (!acc.hasOwnProperty(mapName)) {
+                    acc[mapName] = { "player": 0, "admin": 0 };
                 }
+                acc[mapName][pick.type] = acc[mapName][pick.type] + 1;
             }
             return acc;
         },
         {},
     );
-    const data = [];
+    const player_data = [];
+    const admin_data = [];
     const keys = [];
-    for (const [key, value] of Object.entries(draftPickData).sort(([k, a], [ka, b]) => b - a)) {
-        data.push(value);
+    for (const [key, value] of Object.entries(draftPickData).sort(([k, a], [ka, b]) => b.admin + b.player - a.admin - a.player)) {
+        player_data.push(value.player);
+        admin_data.push(value.admin);
         keys.push(GameNameMappingToDisplayName[mapDraftNameToGameNameMapping[key]]);
     }
-    
+
     const style = getComputedStyle(document.body);
-    const options = merge(MapChartConfig(style), FilterLegendConfig(style, filter, false), {plugins: {
-        title: {display: true, text: 'Maps picks'},
-        plugins: {tooltip: {enables: true}},
-    }});
-    return <Chart data={{datasets: [{data: data}], labels: keys}} options={options}></Chart>;
+    const options = merge(MapChartConfig(style), FilterLegendConfig(style, filter, false), {
+        plugins: {
+            title: { display: true, text: 'Maps picks' },
+            plugins: { tooltip: { enables: true } },
+        },
+        scales: {
+            x: {
+                stacked: true
+            },
+            y: {
+                stacked: true
+            }
+        }
+    });
+    return <Chart data={{
+        datasets: [
+            {
+                label: 'Player picks',
+                data: player_data,
+                backgroundColor: style.getPropertyValue('--ifm-color-primary')
+            },
+            {
+                label: 'Admin picks',
+                data: admin_data,
+                backgroundColor: style.getPropertyValue('--ifm-color-primary-lightest')
+            },
+        ],
+        labels: keys
+    }} options={options}></Chart>;
 };
