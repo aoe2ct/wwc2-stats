178148c85a3c81143812a29610e5cbcabb19f73c
Stéphane Bisinger <stephane@sbisinger.ch>
2024-12-05 19:42:22 +0100

    Show all civs on the civs played chart

--- src/components/charts/civ-play-chart.tsx
+++ src/components/charts/civ-play-chart.tsx
@@ -3,23 +3,27 @@ import Chart from "./chart";
 import useDelayedColorMode from "@site/src/utils/use-delayed-color-mode";
 import { merge } from 'lodash-es';
 import { Filter } from "../filter/filter-dialog";
+import { allCivs } from "@site/src/data/mapping";
 
-export default function CivPlayChart({gamesData, filter}: {gamesData: any[], filter: Filter}): JSX.Element {
+export default function CivPlayChart({ gamesData, filter }: { gamesData: any[], filter: Filter }): JSX.Element {
     useDelayedColorMode();
-    const civPlayData: {[key: string]: number} = gamesData.reduce(
-        (acc, game) => {
-            const civsPlayed = [game.winningCiv, game.losingCiv];
-            for (const civ of civsPlayed) {
-                if (acc.hasOwnProperty(civ)) {
-                    acc[civ] = acc[civ] + 1;
-                } else {
-                    acc[civ] = 1;
+    const basePlayData: { [key: string]: number } = Object.fromEntries(allCivs.map((civ) => [civ, 0]));
+    const civPlayData: { [key: string]: number } = {
+        ...basePlayData, ...gamesData.reduce(
+            (acc, game) => {
+                const civsPlayed = [game.winningCiv, game.losingCiv];
+                for (const civ of civsPlayed) {
+                    if (acc.hasOwnProperty(civ)) {
+                        acc[civ] = acc[civ] + 1;
+                    } else {
+                        acc[civ] = 1;
+                    }
                 }
-            }
-            return acc;
-        },
-        {},
-    );
+                return acc;
+            },
+            {},
+        )
+    };
     const data = [];
     const keys = [];
     for (const [key, value] of Object.entries(civPlayData).sort(([_ka, a], [_kb, b]) => b - a)) {
@@ -28,14 +32,18 @@ export default function CivPlayChart({gamesData, filter}: {gamesData: any[], fil
     }
 
     const style = getComputedStyle(document.body);
-    const options = merge(CivChartConfig(style, data), FilterLegendConfig(style, filter, true), {plugins: {
-        title: {display: true, text: 'Civilizations played'},
-        tooltip: {enables: true},
-    }});
-    return <Chart data={{datasets: [{
-        data: data,
-        backgroundColor: data.map((_v, i) => i % 2 === 0 ? style.getPropertyValue('--ifm-color-primary') : style.getPropertyValue('--ifm-color-secondary')),
-        borderColor: data.map((_v, i) => i % 2 === 0 ? style.getPropertyValue('--ifm-color-primary-dark') : style.getPropertyValue('--ifm-color-secondary-dark')),
-        borderWidth: 2,
-    }], labels: keys}} options={options}></Chart>;
+    const options = merge(CivChartConfig(style, data), FilterLegendConfig(style, filter, true), {
+        plugins: {
+            title: { display: true, text: 'Civilizations played' },
+            tooltip: { enables: true },
+        }
+    });
+    return <Chart data={{
+        datasets: [{
+            data: data,
+            backgroundColor: data.map((_v, i) => i % 2 === 0 ? style.getPropertyValue('--ifm-color-primary') : style.getPropertyValue('--ifm-color-secondary')),
+            borderColor: data.map((_v, i) => i % 2 === 0 ? style.getPropertyValue('--ifm-color-primary-dark') : style.getPropertyValue('--ifm-color-secondary-dark')),
+            borderWidth: 2,
+        }], labels: keys
+    }} options={options}></Chart>;
 };
